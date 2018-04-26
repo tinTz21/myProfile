a4389375665774f0c7c2aa373a12e3e08129a088
Augustino Emanuel <augustino.emanuel73@gmail.com>
2018-04-26 23:43:43 +0000

    first commit

--- resources/views/partials/banners.blade.php
+++ resources/views/partials/banners.blade.php
@@ -1,9 +1,9 @@
-<article class="intro-banner">
+ <article class="intro-banner">
 <div class="container-fluid">
 <div class="jumbotron">
 	<div class="row">
-		<div class="col-sm-5"></div>
-		<div class="col-sm-7">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-8">
 		<h1>We keep your profile</h1>
 		<p>ut lacinia in, elementum id enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sollicitudin molestie malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.</p>
 		</div>
