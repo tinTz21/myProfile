bc8162c6f20c5c70342a553694665014fd3abe53
Augustino Emanuel <augustino.emanuel73@gmail.com>
2018-05-02 11:08:56 +0000

    first commit

--- resources/views/partials/navigationbar.blade.php
+++ resources/views/partials/navigationbar.blade.php
@@ -1,8 +1,14 @@
 
    	<div class="container">
    	<div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#myMenu">
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+      </button>
    		<a class="navbar-brand" href="{{ action('pagesController@index') }}">myPROFILE</a>
    	</div>
+      <div class="collapse navbar-collapse"  id="myMenu">
    	<ul class="nav navbar-nav">
    		<li><a href=" {{ route('pages.index') }} ">Home</a></li>
    		<li><a href="{{ route('pages.create') }}">Publish</a></li>
@@ -18,3 +24,4 @@
    			@endif
    		</ul>
    	</div>
+      </div>
